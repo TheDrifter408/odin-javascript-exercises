8db1bfeca19ea7e01875af664a5addd046a2bef9
TheDrifter408 <nurullahan408@gmail.com>
2024-09-04 20:21:29 +0600

    Completed removeFromArray.js with all test passing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    let allArgs = Array.from(args);
+    for(let i = 0;i<allArgs.length;i++){
+        for(let j=0;j<array.length;j++){
+            if(array.includes(allArgs[i])){
+                let index = array.indexOf(allArgs[i]);
+                array.splice(index,1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
