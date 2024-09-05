2269503bf361b3d570856747956a97cf67e5c844
TheDrifter408 <nurullahan408@gmail.com>
2024-09-05 21:18:06 +0600

    Completed -5_sumAll and all tests completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    let result = 0;
+    if(typeof start === 'string' || typeof end === 'string'){
+        return 'ERROR';
+    }
+    else if(start < 0 || end < 0){
+        return 'ERROR';
+    } 
+    else if(!Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR';
+    } else {
+        if(end < start){
+            let temp = start;
+            start = end;
+            end = temp;
+        }
+        for(let i = start;i <= end;i++){
+            result += i;
+        }
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
