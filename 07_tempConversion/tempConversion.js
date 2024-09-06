3a85390f08e1680e5f4e3caaa2307959cca5fb01
TheDrifter408 <nurullahan408@gmail.com>
2024-09-06 10:44:49 +0600

    Completed 07_tempConversion and passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let temp = (fahrenheit - 32);
+  let celsius = (temp * (5/9)).toFixed(1);
+  return Number(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let temp = (celsius * 9/5);
+  let fahrenheit = (temp + 32).toFixed(1);
+  return Number(fahrenheit);
 };
 
 // Do not edit below this line
