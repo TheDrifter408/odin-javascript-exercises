945e64cddb0451c2a1695703549bb01199cfc27c
TheDrifter408 <nurullahan408@gmail.com>
2024-09-07 20:19:55 +0600

    Completed all the functions and passed the tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if(nums.length === 1){
+    return nums[0];
+  }
+	let total = nums.reduce((total,currentItem) => {
+    return total + currentItem;
+  },0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let total = nums.reduce((total,currentItem) => {
+    return total * currentItem;
+  },1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base,power) {
+  let total = 1;
+  while(power > 0){
+    total *= base;
+    power--;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  if(num === 0){
+    return total;
+  }
+  while(num > 0){
+    total *= num;
+    num--;
+  }
+  return total;
 };
 
 // Do not edit below this line
