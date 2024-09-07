892148a5fcc863254094af69d1e3ac6a371fed62
TheDrifter408 <nurullahan408@gmail.com>
2024-09-07 21:43:03 +0600

    Finished the function and completed all tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(seriesIndex) {
+    let sum = 0;
+    let before = 0;
+    let after = 1;
+    let input = Number(seriesIndex);
+    if (isNaN(input) || input < 0){
+        return "OOPS";
+    } else if(input === 0){
+        return before;
+    } else if(input === 1){
+        return after;
+    } else {
+        let i = 0;
+        while( i <= seriesIndex - 2){
+            sum = before + after;
+            before = after;
+            after = sum;
+            i++;
+        }
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
