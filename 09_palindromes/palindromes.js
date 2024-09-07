e9071e7364b0f34a52d730a506f315318aacb9c5
TheDrifter408 <nurullahan408@gmail.com>
2024-09-07 20:29:31 +0600

    Completed the function and passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let isPalindrome = false;
+    let filtered = word.split("").filter((char) => {
+        return (char !== '!' && char !== "," && char !== " " && char !== ".");
+    }).join("").toLowerCase();
+    let reversed = filtered.split("").reverse().join("");
+    if(reversed === filtered){
+        isPalindrome = true;
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
