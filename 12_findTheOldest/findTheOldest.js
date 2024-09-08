17bd06b40b9b5f80a20a8146bd3ee371fd6a429c
TheDrifter408 <nurullahan408@gmail.com>
2024-09-08 10:29:15 +0600

    Completed the function and pased all tests.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+function age(person){
+    let name = person.name;
+    let age = 0;
+    if(person.hasOwnProperty('yearOfDeath')){
+        age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+        age = new Date().getFullYear() - person.yearOfBirth;
+    }
+    return {
+        name,age
+    }
+}
+const findTheOldest = function(people) {
+    let person = people.map((person) => age(person)).reduce((acc,val) => {
+        return acc.age < val.age ? val : acc;
+    });
+    return person;
 };
 
 // Do not edit below this line
